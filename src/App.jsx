e3a1f74f3e8311af6e5af0b178baa4100d75602c
kemal-ramadhan <km.kemal03@gmail.com>
2025-03-23 17:43:54 +0700

    solve

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Helmet, HelmetProvider } from "react-helmet-async";
-=======
-=======
->>>>>>> 8bbe4dc (First Commit)
-=======
->>>>>>> origin/Budi
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
-import { useEffect } from "react";
-import useGoogleAnalytics, { logPageView } from "./utils/analitics";
-
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 1d241ff (ADD: transition close modal, loading fakultas; FIX: navbar;)
-=======
->>>>>>> 8bbe4dc (First Commit)
-=======
->>>>>>> origin/Budi
 import Home from './pages/Home';
 import Fakultas from "./pages/Fakultas";
 import Jalur from "./pages/Jalur";
@@ -31,37 +11,11 @@ import Faq from "./pages/Faq";
 import Contact from "./pages/Contact";
 import ArtikelDetail from "./pages/ArtikelDetail";
 import Artikel from "./pages/Artikel";
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 function App() {
   return (
     <HelmetProvider>
       <Router>
-=======
-=======
->>>>>>> 8bbe4dc (First Commit)
-=======
->>>>>>> origin/Budi
-import Prestasi from "./pages/Prestasi";
-import Mitra from "./pages/Mitra";
-import Testimoni from "./pages/Testimoni";
-
-function App() {
-  const trackingId = useGoogleAnalytics();
-
-  return (
-    <HelmetProvider>
-      <Router>
-        {trackingId && <GAListener />}
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 1d241ff (ADD: transition close modal, loading fakultas; FIX: navbar;)
-=======
->>>>>>> 8bbe4dc (First Commit)
-=======
->>>>>>> origin/Budi
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/artikel' element={<Artikel />} />
@@ -73,61 +27,12 @@ function App() {
           <Route path='/fasilitas' element={<Fasilitas />} />
           <Route path='/faq' element={<Faq />} />
           <Route path='/kontak' element={<Contact />} />
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-          <Route path='/prestasi' element={<Prestasi />} />
-          <Route path='/mitra' element={<Mitra />} />
-          <Route path='/testimoni' element={<Testimoni />} />
->>>>>>> 1d241ff (ADD: transition close modal, loading fakultas; FIX: navbar;)
-=======
-          <Route path='/prestasi' element={<Prestasi />} />
-          <Route path='/mitra' element={<Mitra />} />
-          <Route path='/testimoni' element={<Testimoni />} />
->>>>>>> 8bbe4dc (First Commit)
-=======
-          <Route path='/prestasi' element={<Prestasi />} />
-          <Route path='/mitra' element={<Mitra />} />
-          <Route path='/testimoni' element={<Testimoni />} />
->>>>>>> origin/Budi
           <Route path='/mahasiswa-dan-alumni' element={<Mahasiswa />} />
         </Routes>
       </Router>
     </HelmetProvider>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 
   )
 }
 
 export default App
-=======
-=======
->>>>>>> 8bbe4dc (First Commit)
-=======
->>>>>>> origin/Budi
-  );
-}
-
-const GAListener = () => {
-  const location = useLocation();
-
-  useEffect(() => {
-    logPageView();
-  }, [location]);
-
-  return null;
-};
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-export default App;
->>>>>>> 1d241ff (ADD: transition close modal, loading fakultas; FIX: navbar;)
-=======
-export default App;
->>>>>>> 8bbe4dc (First Commit)
-=======
-export default App;
->>>>>>> origin/Budi
--- vite.config.js
+++ vite.config.js
@@ -15,29 +15,9 @@ export default defineConfig({
   define: {
     'process.env': {}
   },
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
   server: {
     allowedHosts: ["dd1d-180-244-129-187.ngrok-free.app"], // Tambahkan domain ngrok kamu di sini
     host: true, // Memungkinkan akses dari jaringan luar
     port: 5173, // Pastikan sesuai dengan port yang kamu gunakan
   }
-=======
-=======
->>>>>>> 8bbe4dc (First Commit)
-=======
->>>>>>> origin/Budi
-  // server: {
-  //   allowedHosts: ["dd1d-180-244-129-187.ngrok-free.app"], // Tambahkan domain ngrok kamu di sini
-  //   host: true, // Memungkinkan akses dari jaringan luar
-  //   port: 5173, // Pastikan sesuai dengan port yang kamu gunakan
-  // }
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 1d241ff (ADD: transition close modal, loading fakultas; FIX: navbar;)
-=======
->>>>>>> 8bbe4dc (First Commit)
-=======
->>>>>>> origin/Budi
 })
