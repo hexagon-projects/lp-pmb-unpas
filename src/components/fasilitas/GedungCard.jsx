573039680a85a520f406f9a97b7cfcfc1d5515ac
reygan426 <regan.euy@gmail.com>
2025-04-17 15:32:48 +0700

    fix footer

--- src/components/fasilitas/GedungCard.jsx
+++ src/components/fasilitas/GedungCard.jsx
@@ -7,7 +7,7 @@ const GedungCard = ({ title, text, mapUrl, image, imagedb, index }) => {
     return (
         <div
             className="h-full bg-[#f0f0f0] shadow-black/5 shadow-xl drop-shadow-[0px_20px_40px_rgba(254, 242, 81, 0.5)] rounded-xl md:rounded-2xl lg:rounded-4xl p-4 lg:p-8 space-y-3 relative cursor-pointer flex flex-col justify-between items-stretch group"
-            onClick={() => window.open(mapUrl, "_blank")}
+            // onClick={() => window.open(mapUrl, "_blank")}
         >
             <h2 className="text-sm md:text-base lg:text-lg font-bold">{title}</h2>
             <p className="text-xs md:text-sm">{text}</p>
